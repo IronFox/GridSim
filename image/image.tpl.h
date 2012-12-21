0825669e5a24c12d32ceb031183aa24a1d5bb4ee
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2012-12-21 11:14:38 +0000

    Finished implementation of copyChannel() method in images
    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@56 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- image/image.tpl.h
+++ image/image.tpl.h
@@ -784,22 +784,20 @@ template <class Nature>
 		}
 
 template <typename T>
-	void			ImageTemplate<T>::copyChannel(copyChannel(const ImageTemplate<T>&source_image, BYTE source_channel, BYTE target_channel)
+	void			ImageTemplate<T>::copyChannel(const ImageTemplate<T>&source_image, BYTE source_channel, BYTE target_channel)
 	{
 		if (source_image.width() != width() || source_image.height() != height() || source_channel >= source_image.channels() || target_channel >= channels())
 		{
 			FATAL__("Copy operation cannot be performed due to image incompatibility or parameter invalidity");
 			return;
 		}
-		Concurrency::parallel_for(dimension_t(0),image_width,[this,&buffer](dimension_t x)
+		Concurrency::parallel_for(dimension_t(0),image_width,[this,&source_image,source_channel,target_channel](dimension_t x)
 		{
 			for (dimension_t y = 0; y < image_height; y++)
 			{
-				//   BYTE*old = getPixel(x,y);
-				Vec::copyD(this->getPixel(x,y),&buffer[size_type(y)*size_type(image_width)*size_type(4)+size_type(x)*size_type(4)],vmin(image_channels,4));
+				get(x,y)[target_channel] = source_image.get(x,y)[source_channel];
 			};
 		});
-
 	}
 
 template <typename T>
