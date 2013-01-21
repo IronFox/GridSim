aeaf48bdfcceee985a01f6cead4303f81bfad029
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2013-01-21 16:47:53 +0000

    Set Engine::timing.now64 to use timer.now() instead of the counter variable starting at program start.
    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@203 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- engine/timing.cpp
+++ engine/timing.cpp
@@ -5,11 +5,6 @@
 
 Engine timing-unit.
 
-This file is part of Delta-Works
-Copyright (C) 2006-2008 Stefan Elsen, University of Trier, Germany.
-http://www.delta-works.org/forge/
-http://informatik.uni-trier.de/
-
 ******************************************************************/
 
 namespace Engine
@@ -22,7 +17,7 @@ namespace Engine
 
 	void Timing::update()
 	{
-	    delta64 = timer.getTimei()-now64;
+	    delta64 = timer.now()-now64;
 	    now64+=delta64;
 	    delta = (float)delta64/ticks_per_second;
 	    now = (double)now64/ticks_per_second;
@@ -33,5 +28,6 @@ namespace Engine
 		ticks_per_second = timer.getTicksPerSecond();
 		delta = 0.01f;
 		delta64 = 1;
+		now64 = timer.now();
 	}
 }
