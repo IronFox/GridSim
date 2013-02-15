a75be1fda68b8e20b5dede09883693b332fd2ebf
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2013-02-15 11:28:36 +0000

    Application config file is generated when not found now. Using lower case filenames for application config xml files.
    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@308 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- engine/auto_display_configure.h
+++ engine/auto_display_configure.h
@@ -13,44 +13,59 @@ namespace Engine
 		return Template::isalnum(c) || c == ' ' || c == '_';
 	}
 
+	namespace Detail
+	{
 	template <typename GL>
-		void	createDisplay(Display<GL>&display, const String&name, Resolution resolution, DisplayConfig::f_on_resize onResize)
+		void	UpdateXML(Display<GL>&display, const String&filename)
 		{
-			String file_name = name;
-			file_name.eraseCharacters(validFileNameChar,false);
 			XML::Container xconfig;
+			XML::Node&xdisplay = xconfig.create("config/display");
+			xdisplay.set("width",display.width());
+			xdisplay.set("height",display.height());
+			FileSystem::createFolder("config");
+			xconfig.saveToFile("./config/"+filename+".xml");
+		}
+	}
+
+	template <typename GL>
+		void	CreateDisplay(Display<GL>&display, const String&name, Resolution resolution, DisplayConfig::f_on_resize onResize)
+		{
+			String fileName = name;
+			fileName.eraseCharacters(validFileNameChar,false);
+			fileName.convertToLowerCase();
+			XML::Container xconfig;
+			bool updateFile = false;
 			try
 			{
-				xconfig.loadFromFile("./config/"+file_name+".xml");
+				xconfig.loadFromFile("./config/"+fileName+".xml");
 				XML::Node*xdisplay = xconfig.find("config/display");
 				if (xdisplay)
 				{
 					String value;
-					if (xdisplay->query("width",value))
-						convert(value.c_str(),resolution.width);
-					if (xdisplay->query("height",value))
-						convert(value.c_str(),resolution.height);
+					if (!xdisplay->query("width",value)|| !convert(value.c_str(),resolution.width))
+						updateFile = true;
+					
+					if (!xdisplay->query("height",value)|| !convert(value.c_str(),resolution.height))
+						updateFile = true;
 				}
 			}
 			catch (const IO::DriveAccess::FileOpenFault&)
-			{}
+			{
+				updateFile = true;
+			}
 			
-			DisplayConfig config(name,[file_name,onResize,&display](UINT width, UINT height, bool is_final, bool is_full_screen){
+			DisplayConfig config(name,[fileName,onResize,&display](UINT width, UINT height, bool is_final, bool is_full_screen){
 				if (is_final && !is_full_screen)
 				{
-					XML::Container xconfig;
-					XML::Node&xdisplay = xconfig.create("config/display");
-					xdisplay.set("width",display.width());
-					xdisplay.set("height",display.height());
-					FileSystem::createFolder("config");
-					xconfig.saveToFile("./config/"+file_name+".xml");
+					Detail::UpdateXML(display,fileName);
 				}
 				onResize(width,height,is_final,is_full_screen);
 			});
 			display.setSize(resolution.width, resolution.height, DisplayConfig::ResizableBorder);
-			
 			if (!display.create(config))
 				FATAL__("Unable to create window (" + display.errorStr() + ")");
+			if (updateFile)
+				Detail::UpdateXML(display,fileName);
 		}
 		
 		
