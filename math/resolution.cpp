a89e6457e49a73ddc5d526b283cf95c36e34e3e2
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2014-02-05 18:28:42 +0000

    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@933 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- math/resolution.cpp
+++ math/resolution.cpp
@@ -1,8 +1,19 @@
 #include "../global_root.h"
 #include "resolution.h"
 
+
+Resolution::Resolution(const Resolution3D&res):width(res.width),height(res.height)
+{
+//	DBG_ASSERT__(res.depth == 1);	going to tolerate this for now
+}
+
+
 hash_t Resolution::Hash::hash(const Resolution&res)
 {
 	return stdMemHashT<sizeof(Resolution)>(&res);
 }
 
+hash_t Resolution3D::Hash::hash(const Resolution3D&res)
+{
+	return stdMemHashT<sizeof(Resolution3D)>(&res);
+}
--- math/resolution.h
+++ math/resolution.h
@@ -3,6 +3,9 @@
 
 #include "../interface/hashable.h"
 
+
+struct Resolution3D;
+
 struct Resolution
 {
 public:
@@ -20,6 +23,7 @@ public:
 									{}
 									Resolution(UINT width_,UINT height_):width(width_),height(height_)
 									{}
+		explicit					Resolution(const Resolution3D&);
 		void						set(UINT width_, UINT height_)
 									{
 										width = width_;
@@ -59,6 +63,68 @@ public:
 									}
 };
 
+struct Resolution3D
+{
+public:
+		struct Hash
+		{
+		static	hash_t				hash(const Resolution3D&res);
+		};
+
+
+		UINT32						width,
+									height,
+									depth;
+
+
+									Resolution3D():width(0),height(0),depth(0)
+									{}
+									Resolution3D(UINT width,UINT height, UINT depth):width(width),height(height),depth(depth)
+									{}
+									Resolution3D(const Resolution&res, UINT depth):width(res.width),height(res.height),depth(depth)
+									{}
+		void						Set(UINT width_, UINT height_, UINT depth_)
+									{
+										width = width_;
+										height = height_;
+										depth = depth_;
+									}
+
+		bool						operator==(const Resolution3D&other)	const
+									{
+										return width == other.width && height == other.height && depth == other.depth;
+									}
+		bool						operator!=(const Resolution3D&other)	const
+									{
+										return width != other.width || height != other.height || depth != other.depth;
+									}
+		Resolution3D				operator/(int div) const
+									{
+										return Resolution3D(std::max<UINT32>(1,width/div),std::max<UINT32>(1,height/div),std::max<UINT32>(1,depth/div));
+									}
+		Resolution3D&				operator/=(int div)
+									{
+										width = std::max<UINT32>(1,width/div);
+										height = std::max<UINT32>(1,height/div);
+										depth = std::max<UINT32>(1,depth/div);
+										return *this;
+									}
+		Resolution3D				operator*(int factor) const
+									{
+										return Resolution3D(
+												std::max<UINT32>(1,width*factor),
+												std::max<UINT32>(1,height*factor),
+												std::max<UINT32>(1,depth*factor)
+												);
+									}
+		Resolution3D&				operator*=(int factor)
+									{
+										width = std::max<UINT32>(1,width*factor);
+										height = std::max<UINT32>(1,height*factor);
+										depth = std::max<UINT32>(1,depth*factor);
+										return *this;
+									}
+};
 
 
 
