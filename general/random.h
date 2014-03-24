6e22c7f1e5dced53033e65e7842a6333ee8a5d29
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2014-03-24 17:50:20 +0000

    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@988 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- general/random.h
+++ general/random.h
@@ -9,11 +9,6 @@
 
 E:\include\general\random.h
 
-This file is part of Delta-Works
-Copyright (C) 2006-2008 Stefan Elsen, University of Trier, Germany.
-http://www.delta-works.org/forge/
-http://informatik.uni-trier.de/
-
 ******************************************************************/
 
 /*!
