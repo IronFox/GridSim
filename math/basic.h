8629f582fe571d5489e54bd3fe75a21ea9b8ad57
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2014-07-02 11:43:59 +0000

    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@1062 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- math/basic.h
+++ math/basic.h
@@ -227,7 +227,7 @@ namespace Math
 		BYTE	value;
 	public:
 		/**/	Bit(int val = 0) : value(val ? 1 : 0)	{DBG_ASSERT__(val == 0 || val == 1);}
-		Bit&	operator=(int val) {DBG_ASSERT__(val == 0 || val == 1); value = val ? 1 : 0;}
+		Bit&	operator=(int val) {DBG_ASSERT__(val == 0 || val == 1); value = val ? 1 : 0;return *this;}
 		operator int() const {return (int)value;}
 	};
 
