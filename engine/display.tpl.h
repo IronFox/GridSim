332fc1af99eceff56555022870a10ef40c5a7cae
elsen <elsen@87af6b1e-9b34-e211-997a-bc305bce1b9a>
2015-02-21 10:19:18 +0000

    git-svn-id: https://svn.syssoft.uni-trier.de/svn/prisma/DeltaWorks/include@1730 87af6b1e-9b34-e211-997a-bc305bce1b9a

--- engine/display.tpl.h
+++ engine/display.tpl.h
@@ -657,18 +657,21 @@ namespace Engine
 				if (mouse.isLocked())
 					context.checkFocus();
 			}
-	        timing.update();
 			if (context.isMinimized())
 			{
 				Sleep(50.f);
 				if (++callAnyway > 10)
 				{
 					callAnyway = 0;
+					timing.update();
 					exec_loop = !context.shutting_down && exec_target();
 				}
 			}
 			else
-		        exec_loop = !context.shutting_down && exec_target();
+			{
+				timing.update();
+				exec_loop = !context.shutting_down && exec_target();
+			}
 
 	        mouse.update();	//set here to reset motion
 			interruptCheckEvents();
@@ -687,6 +690,8 @@ namespace Engine
 	            return;
 	    #endif
 		BYTE focus_check = 1;
+		BYTE callAnyway = 0;
+
 	    while (exec_loop)
 	    {
 			if (!--focus_check)
@@ -695,11 +700,22 @@ namespace Engine
 				if (mouse.isLocked())
 					context.checkFocus();
 			}
-	        timing.update();
 			if (context.isMinimized())
+			{
 				Sleep(50.f);
+				if (++callAnyway > 10)
+				{
+					callAnyway = 0;
+					timing.update();
+					exec_loop = !context.shutting_down && exec_target();
+				}
+			}
 			else
-		        exec_loop = !context.shutting_down && exec_target();
+			{
+				timing.update();
+				exec_loop = !context.shutting_down && exec_target();
+			}
+
 
 	        mouse.update();
 
