e2ecc64849f061b8fecbd61d7e5c67df94b4220d
IronFox <elsen@syssoft.uni-trier.de>
2016-08-22 14:37:54 +0200

    OpenGL texture-font2 now correctly uses trilinear filtering

--- engine/gl/texture_font2.cpp
+++ engine/gl/texture_font2.cpp
@@ -126,6 +126,9 @@ namespace Engine
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
 		gluBuild2DMipmaps(GL_TEXTURE_2D,4,image.getWidth(),image.getHeight(),GL_RGBA,GL_UNSIGNED_BYTE,image.getData());
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
 		glBindTexture(GL_TEXTURE_2D,0);
 		
 		base = glGenLists(0x100);
