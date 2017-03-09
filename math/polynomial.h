31882f0199ffe7d42a883cb60d1d881d1fff8905
IronFox <github@portal.delta-works.org>
2017-03-09 09:53:04 +0100

    Added regular 2D polynomial type. Polynomials now accept ArrayRef<> for approximation

--- math/polynomial.h
+++ math/polynomial.h
@@ -4,6 +4,94 @@
 
 namespace Math
 {
+
+	/**
+	N*Mth degree polynomial representation, using negative exponents
+	*/
+	template <typename T, count_t NumCoefX, count_t NumCoefY>
+		struct Polynomial2D
+		{
+			static const constexpr count_t NumCoefficientsX = NumCoefX;
+			static const constexpr count_t NumCoefficientsY = NumCoefY;
+			TMatrix<T,NumCoefX,NumCoefY> coefficients;
+
+			T			operator()(const T x, const T y) const
+			{
+				T z = 0;
+				T y1 = 1;
+				for (index_t iy = 0; iy < NumCoefY; iy++)
+				{
+					T x1 = 1;
+					for (index_t ix = 0; ix < NumCoefX; ix++)
+					{
+						z += coefficients.axis[ix].v[iy] * y1 * x1;
+						x1 *= x;
+					}
+					y1 *= y;
+				}
+				return z;
+			}
+
+			/**
+			Matches the local polynomial to match the given set of points.
+			The X-coordinates of all points must be pairwise different.
+			*/
+			bool			MatchPoints(const std::array<TVec3<T>,NumCoefX*NumCoefY>&points)
+			{
+				static const count_t TotCoef =  NumCoefX * NumCoefY;
+				TMatrix<T,TotCoef,TotCoef>	mX,mInv;
+				TVec<T,TotCoef>	vY;
+
+				for (index_t at = 0; at < TotCoef; at++)
+				{
+					const TVec3<T>&p = points[at];
+					vY.v[at] = p.z;
+
+					const T fx = p.x;
+					const T fy = p.y;
+					for (index_t eX = 0; eX < NumCoefX; eX++)
+					for (index_t eY = 0; eY < NumCoefY; eY++)
+					{
+						const index_t eat = eX * NumCoefY + eY;
+						T xN = vpow(fx,eX) * vpow(fy,eY);
+						mX.axis[eat].v[at] = xN;
+					}
+				}
+				if (!Mat::Invert(mX,mInv))
+					return false;
+				Mat::Mult(mInv,vY, this->coefficients.asVector);
+				return true;
+			}
+			
+			bool		Approximate(const ArrayRef<TVec3<T> >&points)
+			{
+				static const count_t TotCoef =  NumCoefX * NumCoefY;
+
+				Array2D<T> XT(TotCoef,points.Count()),X(points.Count(),TotCoef);
+				Array<T> yv(points.Count());
+				for (index_t i = 0; i < points.Count(); i++)
+				{
+					const TVec3<T>&p = points[i];
+					yv[i] = p.z;
+					for (index_t eX = 0; eX < NumCoefX; eX++)
+					for (index_t eY = 0; eY < NumCoefY; eY++)
+					{
+						const index_t eat = eX * NumCoefY + eY;
+						T xN = vpow(p.x,eX) * vpow(p.y,eY);
+						X.Get(i,eat) = XT.Get(eat,i) = xN;
+					}
+				}
+				Array2D<T> product{TotCoef,TotCoef},inverted{TotCoef,TotCoef},finalProduct{TotCoef,points.Count()};
+				Mat::Raw::MultD(XT.pointer(),X.pointer(),product.pointer(),TotCoef,points.Count(),TotCoef);
+				bool success = Mat::Raw::InvertD(product.pointer(),inverted.pointer(),TotCoef);
+				if (!success)
+					return false;
+				Mat::Raw::MultD(inverted.pointer(),XT.pointer(),finalProduct.pointer(),TotCoef,TotCoef,points.Count());
+				Mat::Raw::MultD(finalProduct.pointer(),yv.pointer(),coefficients.v,TotCoef,points.Count(),1);
+				return true;
+			}
+
+		};
 	
 	
 	/**
@@ -56,7 +144,7 @@ namespace Math
 
 
 			
-			bool		Approximate(const ArrayData<TVec2<T> >&points)
+			bool		Approximate(const ArrayRef<TVec2<T> >&points)
 			{
 				Array2D<T> XT(NumCoef,points.Count()),X(points.Count(),NumCoef);
 				Array<T> yv(points.Count());
@@ -132,7 +220,7 @@ namespace Math
 				return true;
 			}
 			
-			bool		Approximate(const ArrayData<TVec2<T> >&points)
+			bool		Approximate(const ArrayRef<TVec2<T> >&points)
 			{
 				Array2D<T> XT(NumCoef,points.Count()),X(points.Count(),NumCoef);
 				Array<T> yv(points.Count());
@@ -218,7 +306,7 @@ namespace Math
 				return true;
 			}
 			
-			bool		Approximate(const ArrayData<TVec3<T> >&points)
+			bool		Approximate(const ArrayRef<TVec3<T> >&points)
 			{
 				static const count_t TotCoef =  NumCoefX * NumCoefY;
 
