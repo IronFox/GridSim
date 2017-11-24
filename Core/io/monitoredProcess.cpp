e00fe599948318627a8ab7ffedc39fc7730e3ba1
Stefan Elsen <github@portal.delta-works.org>
2017-11-24 12:17:49 +0100

    Monitored Process: Fixed Process termination loop

--- Core/io/monitoredProcess.cpp
+++ Core/io/monitoredProcess.cpp
@@ -465,7 +465,7 @@ namespace DeltaWorks
 					//PostThreadMessageA(threadID,WM_CLOSE,0,0);
 				}
 				count_t numTries = 0;
-				if (WaitForSingleObject(infoOut.hProcess,2000) != 0)
+				while (WaitForSingleObject(infoOut.hProcess,2000) != 0)
 				{
 					if (++numTries > 3)
 						throw Except::Program::GeneralFault(CLOCATION,"Unable to terminate monitored process");
