2a986b7d63ce08980a94060576b7340cf16fb6c9
IronFox <github@portal.delta-works.org>
2018-09-05 19:33:21 +0200

    TexUpdater: Revised output. Added more output files

--- TexUpdater/Program.cs
+++ TexUpdater/Program.cs
@@ -10,6 +10,7 @@ namespace TexUpdater
 {
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
 			var input = new DirectoryInfo("input");
@@ -17,7 +18,7 @@ namespace TexUpdater
 				throw new ArgumentException("No input directory found");
 			var files = input.GetFiles("mergeMeasurements*.xml");
 			if (files.Length != 1)
-				throw new ArgumentException("input directory does not contain exactly one xml file, but "+files.Length);
+				throw new ArgumentException("input directory does not contain exactly one xml file, but " + files.Length);
 
 			MetricTable table = new MetricTable(files[0]);
 
@@ -35,7 +36,7 @@ namespace TexUpdater
 				var _f = Directory.GetFiles("output");
 				foreach (var f in _f)
 				{
-					Console.WriteLine("Erasing old output " + f+"...");
+					Console.WriteLine("Erasing old output " + f + "...");
 					File.Delete(f);
 				}
 			}
@@ -43,67 +44,129 @@ namespace TexUpdater
 			{
 				using (StreamWriter file = new StreamWriter(@"output\statistics.tex"))
 				{
-					/*
-						\begin{tabular}{lll}
-							Property & Mean & Standard Deviation\\
-							$\Omega$ & $1.3_{(\pm 2.8\mathrm{e}{-3})} R$ & $1.7 R$\\
-							$\Delta p(e)$ & $0.16_{(\pm 7.4\mathrm{e}{-4})} R$& $0.44 R$\\
-							$\left| IS \right| \over \left| SD \right|$ & $12_{(\pm 2.5\mathrm{e}{-2})} \%$ & $15\%$\\
-							$\left| IS_{\text{merged}} \right| \over \left| SD \right|$ &
-							$4.4_{(\pm 3.1\mathrm{e}{\text{-}2})} \%$ & $13 \%$\\
-							$P_I$ & $79_{(\pm 7.2\mathrm{e}{-2})} \%$ & $43\%$\\
-							$P_M$ & $16_{(\pm 6\mathrm{e}{-2})} \%$ & $36\%$ \\
-							$P_U$ & $8.5_{(\pm 4.2\mathrm{e}{-2})}\%$ & $24\%$\\
-						\end{tabular}
-					 */
-
 					file.WriteLine(@"\begin{tabular}{lllll}");
+					file.WriteLine(@"& Operational Mean & $\leftarrow \cap IS_M$ & Population Mean & $\leftarrow \cap IS_M$\\");
 					{
-						file.WriteLine(@"Property & Mean(O) & Deviation(O) & Mean(P) & Deviation(P)\\");
-						WriteProperty(file, @"\Omega", "I_Omega", general,8.0,"R");
-						WriteProperty(file, @"\Delta p(e)", "C_SpatialDelta", general,8.0,"R");
-						WriteProperty(file, @"\left| IS \right| \over \left| SD \right|", "ICSize", general,100.0 / 64, @"\%");
-						WriteProperty(file, @"\left| IS_{\text{merged}} \right| \over \left| SD \right|", "ICSize", captures[0],100.0 / 64, @"\%");
-						WriteProperty(file, @"P_I", "C_InconsistencyProbability", general,100.0, @"\%");
-						WriteProperty(file, @"P_M", "C_MissingProbability", general, 100.0, @"\%");
-						WriteProperty(file, @"P_U", "I_UnwantedProbability", general, 100.0, @"\%");
+						var data = Array(general, average);
+						WriteProperty(file, @"\Omega", "I_Omega", data, 8.0, "R");
+						WriteProperty(file, @"\Delta p(e)", "C_SpatialDelta", data, 8.0, "R");
+						WriteProperty(file, @"P_I", "C_InconsistencyProbability", data, 100.0, @"\%");
+						WriteProperty(file, @"P_M", "C_MissingProbability", data, 100.0, @"\%");
+						WriteProperty(file, @"P_U", "I_UnwantedProbability", data, 100.0, @"\%");
+					}
+					file.WriteLine(@"\end{tabular}");
+				}
+			}
+			{
+				using (StreamWriter file = new StreamWriter(@"output\ic_statistics.tex"))
+				{
+					file.WriteLine(@"\begin{tabular}{ll}");
+					file.WriteLine(@"& Mean\\");
+					//file.WriteLine(@"$\left| IS \right| \over \left| SD \right|$ &  $\left| IS_M \right| \over \left| SD \right|$  \\");
+					WriteProperty(file, @"\left| IS \right| \over \left| SD \right|", "ICSize", Array(general), 100.0 / 64, @"\%",true,false);
+					WriteProperty(file, @"\left| IS_M \right| \over \left| SD \right|", "ICSize", Array(captures[0]), 100.0 / 64, @"\%",true,false);
+					file.WriteLine(@"\end{tabular}");
+				}
+			}
+			{
+				using (StreamWriter file = new StreamWriter(@"output\better_worse_statistics.tex"))
+				{
+					file.WriteLine(@"\begin{tabular}{lllll}");
+					file.WriteLine(@"& Mean Better(O) & Mean Worse(O) & Mean Better(P) & Mean Worse(P) \\");
+					{
+						var data = Array(better, worse);
+						WriteProperty(file, @"\Omega", "I_Omega", data, 8.0, "R");
+						WriteProperty(file, @"\Delta p(e)", "C_SpatialDelta", data, 8.0, "R");
+						WriteProperty(file, @"P_I", "C_InconsistencyProbability", data, 100.0, @"\%");
+						WriteProperty(file, @"P_M", "C_MissingProbability", data, 100.0, @"\%");
+						WriteProperty(file, @"P_U", "I_UnwantedProbability", data, 100.0, @"\%");
 					}
 					file.WriteLine(@"\end{tabular}");
 				}
 			}
 
-
-
 			bool brk = true;
 		}
 
-		private static void WriteProperty(StreamWriter file, string caption, string key, MetricTable.CapturePair general, double scale, string unit)
+		private static T[] Array<T>(T element)
 		{
-			file.Write("$");
-			file.Write(caption);
-			file.Write("$");
-			var mean = general.Mean.Measurements[key];
-			var full = general.Full.Measurements[key];
-			WriteProperties(file, mean.Properties,scale,unit);
-			WriteProperties(file, full.Properties, scale, unit);
+			return new T[] { element };
+		}
+
+		private static T[] Array<T>(T a, T b)
+		{
+			return new T[] { a, b };
+		}
+
+		private static T[] ArrayN<T>(T first, bool appendNull) where T : class
+		{
+			return appendNull ? new T[] { first, null } : new T[] { first };
+		}
+
+		static bool putAnd = false;//put & before next cell
+
+		private static void WriteProperty(StreamWriter file, string caption, string key, IEnumerable<MetricTable.CapturePair> sources, double scale, string unit, bool mean = true, bool full = true)
+		{
+			if (caption != null)
+			{
+				file.Write("$");
+				file.Write(caption);
+				file.Write("$");
+				putAnd = true;
+			}
+			else
+				putAnd = false;
+			if (full)
+				foreach (var s in sources)
+					if (s != null)
+						WriteProperties(file, s.Full.Measurements[key].Properties, scale, unit);
+					else
+						WriteEmpty(file);
+			if (mean)
+				foreach (var s in sources)
+					if (s != null)
+						WriteProperties(file, s.Mean.Measurements[key].Properties, scale, unit);
+					else
+						WriteEmpty(file);
+
 			file.WriteLine(@"\\");
+		}
+
+		private static void WriteEmpty(StreamWriter file)
+		{
+			file.Write("&");
+			//if (writeDeviation)
+			//	file.Write("&");
 		}
 
 		private static void WriteProperties(StreamWriter file, MetricTable.MeasurementProperties properties, double scale, string unit)
 		{
-			file.Write("& $");
+			if (putAnd)
+				file.Write("&");
+			file.Write(" $");
 			WriteTexNumber(file, properties.Mean * scale, unit);
-			file.Write(@"_{\pm ");
+			file.Write(@"_{\:\pm ");
 			WriteTexNumber(file, properties.Error * scale, "");
-			file.Write("}$ & $");
-			WriteTexNumber(file, properties.Deviation * scale, unit);
-			file.Write("$");
+			file.Write("}$");
+			//if (writeDeviation)
+			//{
+			//	file.Write("& $");
+			//	WriteTexNumber(file, properties.Deviation * scale, unit);
+			//	file.Write("$");
+			//}
+
+			putAnd = true;
 		}
 
 		private static void WriteTexNumber(StreamWriter file, double v, string unit)
 		{
-			int digits = (int)Math.Floor(Math.Log10(Math.Abs(v))) - 1;
-			double ex = Math.Pow(10.0, digits);
+			int digits = (int)Math.Floor(Math.Log10(Math.Abs(v)));
+			int firstDigit = (int)Math.Round(v / Math.Pow(10.0, digits));
+
+			int zeroDigits = digits - 1;
+			if (firstDigit < 5)
+				zeroDigits--;
+			double ex = Math.Pow(10.0, zeroDigits);
 			v = (Math.Round(v / ex) * ex);
 			string s;
 			if (v < 0.001 || v > 1000)
